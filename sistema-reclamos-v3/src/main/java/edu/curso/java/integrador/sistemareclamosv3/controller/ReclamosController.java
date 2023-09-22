c727122ed34e8fa6be6054c02fec7d9b9749e3f6
Claudio Zamoszczyk <claudio.zamoszczyk@gmail.com>
2023-09-22 11:28:43 -0300

    ejemplo de alta

--- sistema-reclamos-v3/src/main/java/edu/curso/java/integrador/sistemareclamosv3/controller/ReclamosController.java
+++ sistema-reclamos-v3/src/main/java/edu/curso/java/integrador/sistemareclamosv3/controller/ReclamosController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import edu.curso.java.integrador.sistemareclamosv3.bo.Reclamo;
 import edu.curso.java.integrador.sistemareclamosv3.service.ReclamoService;
@@ -36,6 +37,43 @@ public class ReclamosController {
 	@RequestMapping(value = "/borrar/{id}") //URL FINAL /reclamos/borrar/1233434543645
 	public String borrarReclamo(Model model,@PathVariable  Long id) {
 		reclamoService.borrarReclamo(id);
+		return "redirect:/reclamos/listar";
+	}
+
+	@RequestMapping(value = "/nuevo")
+	public String nuevoReclamo(Model model) {
+		ReclamoForm reclamoForm = new ReclamoForm();
+		model.addAttribute("reclamoForm", reclamoForm);
+		return "/reclamos/form";
+	}
+
+	@RequestMapping(value = "/editar/{id}")
+	public String editarReclamo(Model model, @PathVariable  Long id) {
+		Reclamo reclamo = reclamoService.buscarReclamoPorId(id);
+		ReclamoForm reclamoForm = new ReclamoForm();
+		reclamoForm.setId(reclamo.getId());
+		reclamoForm.setTitulo(reclamo.getTitulo());
+		reclamoForm.setDescripcion(reclamo.getDescripcion());
+		model.addAttribute("reclamoForm", reclamoForm);
+		return null;
+	}
+
+	@RequestMapping(value = "/guardar", method = RequestMethod.POST)
+	public String guardarReclamo(Model model, ReclamoForm reclamoForm) {
+		Long idActual = reclamoForm.getId();
+		
+		if(idActual == null) {
+			Reclamo reclamo = new Reclamo();
+			reclamo.setTitulo(reclamoForm.getTitulo());
+			reclamo.setDescripcion(reclamoForm.getDescripcion());
+			reclamoService.altaDeNuevoReclamo(reclamo);
+		} else {
+			Reclamo reclamo = reclamoService.buscarReclamoPorId(idActual);
+			reclamo.setTitulo(reclamoForm.getTitulo());
+			reclamo.setDescripcion(reclamoForm.getDescripcion());
+			reclamoService.actualizarReclamo(reclamo);
+		}
+		
 		return "redirect:/reclamos/listar";
 	}
 
